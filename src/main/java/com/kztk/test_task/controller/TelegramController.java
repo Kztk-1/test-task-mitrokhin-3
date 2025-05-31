d8140160de911c75cc8cb5786a8851bb04e7ac3d
KoZZZtik-3d <kv.sobolev06@gmail.com>
2025-05-31 14:25:37 +0300

    test

--- src/main/java/com/kztk/test_task/controller/TelegramController.java
+++ src/main/java/com/kztk/test_task/controller/TelegramController.java
@@ -1,12 +1,10 @@
 package com.kztk.test_task.controller;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import jakarta.servlet.http.HttpServletRequest;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.Map;
 
@@ -16,9 +14,13 @@ public class TelegramController {
     private static final Logger logger = LoggerFactory.getLogger(TelegramController.class);
 
     @PostMapping("/webhook")
+    @CrossOrigin(origins = "*")
     public void handleWebAppData(
+            HttpServletRequest request,
             @RequestBody(required = false) String rawData,
             @RequestParam Map<String, String> allParams) {
+
+        logger.info("Incoming request: {} {}", request.getMethod(), request.getRequestURI());
 
         logger.info("\n=== Received Telegram WebApp Data ===");
 
